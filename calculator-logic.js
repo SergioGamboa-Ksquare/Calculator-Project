db138e752e175718228d83cfff99bd5984da19b0
SergioGamboa-Ksquare <102177384+SergioGamboa-Ksquare@users.noreply.github.com>
2022-04-09 22:44:48 -0500

    Update calculator-logic.js

--- calculator-logic.js
+++ calculator-logic.js
@@ -1,16 +1,80 @@
-///
-///
+let dotCounter = false;
+
 
 function buttonFun(input) {
   let screen = document.getElementById("screen").value;
-  screen = screen + input;
-  if (screen == "" && input == 0) {
-   } else if (input == "." && screen.includes(".")) {
-  }else if (input == "c") {
-    //c erase screen
+  // Inputing screeen with general numbers
+  if (input == "c") {
+    dotCounter = false;
     screen = null;
     document.getElementById("screen").value = screen;
-  } else
-  document.getElementById("screen").value = screen;
-  //screen += screen.setAttribute("value", input);
+  } else {
+    screen = screen + input;
+    document.getElementById("screen").value = screen;
+  }
+}
+
+function buttonOp(operator) {
+  let screen = document.getElementById("screen").value;
+
+  switch (operator) {
+    case "+":
+      dotCounter = false;
+      console.log(dotCounter);
+      if (operator == "+" && screen.includes("+")) {
+        console.log(screen.split(operator));
+        GeneralEquals(screen, operator);
+      } else {
+        console.log(screen.split(operator));
+
+        Add(screen, operator);
+      }
+      break;
+    case "-":
+      dotCounter = false;
+      if (operator == "-" && screen.includes("-")) {
+        console.log(screen.split(operator));
+        GeneralEquals(screen, operator);
+      } else {
+        console.log(screen.split(operator));
+        Minus(screen, operator);
+      }
+      break;
+    case "*":
+      dotCounter = false;
+
+      if (operator == "*" && screen.includes("*")) {
+        console.log(screen.split(operator));
+        GeneralEquals(screen, operator);
+      } else {
+        console.log(screen.split(operator));
+        Times(screen, operator);
+      }
+      break;
+    case "/":
+      dotCounter = false;
+
+      if (operator == "/" && screen.includes("/")) {
+        console.log(screen.split(operator));
+        GeneralEquals(screen, operator);
+      } else {
+        console.log(screen.split(operator));
+        Times(screen, operator);
+      }
+      break;
+    case ".":
+      if (dotCounter == true) {
+      } else {
+        dotCounter = true;
+        screen = screen + operator;
+        document.getElementById("screen").value = screen;
+      }
+
+      break;
+    case "=":
+      Equals(screen, operator);
+      break;
+
+    default:
+  }
 }
